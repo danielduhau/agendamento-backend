54e5a76b30ae2b28ef6337e0fba687d3fc247a65
daniel.manhaes <daniel.manhaes@prodam.am.gov.br>
2023-01-09 18:03:03 -0400

    Initial commit

---
+++ agendamento-backend/src/test/java/com/danielduhau/agendamentobackend/AgendamentoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielduhau.agendamentobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
